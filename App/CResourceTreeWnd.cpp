db5c1b47f8e3bca37982dfde6537ea7f7faa2edd
shuangshaohen <yu_chuankun@126.com>
2020-04-16 17:17:36 +0800

    修改树结构

--- App/CResourceTreeWnd.cpp
+++ App/CResourceTreeWnd.cpp
@@ -20,6 +20,36 @@ CResourceTreeWnd::CResourceTreeWnd(QWidget *parent): QTreeWidget(parent)
     connect( this, SIGNAL(currentItemChanged( QTreeWidgetItem *, QTreeWidgetItem * )), this, SLOT(currentItemChangedSlot( QTreeWidgetItem *, QTreeWidgetItem * )));
 }
 
+CResourceTreeWnd::~CResourceTreeWnd()
+{
+    delete m_pRootTreeWidgetItem;
+    delete m_pInfoTreeWidgetItem;
+    delete m_pParasTreeWidgetItem;
+    delete m_pSmpTreeWidgetItem;
+    delete m_pAnaTreeWidgetItem;
+    delete m_pDerivedTreeWidgetItem;
+    delete m_pSvTreeWidgetItem;
+    delete m_pGsAnaTreeWidgetItem;
+    delete m_pOherAnaTreeWidgetItem;
+    delete m_pBITreeWidgetItem;
+    delete m_pGeneralBITreeWidgetItem;
+    delete m_pSignalTreeWidgetItem;
+    delete m_pGSBITreeWidgetItem;
+    delete m_pSoftYBTreeWidgetItem;
+    delete m_pBOTreeWidgetItem;
+    delete m_pGSBOTreeWidgetItem;
+    delete m_pTripMatrixTreeWidgetItem;
+    delete m_pYKTreeWidgetItem;
+    delete m_pSetTreeWidgetItem;
+    delete m_pSPSetTreeWidgetItem;
+    delete m_pSGSetTreeWidgetItem;
+    delete m_pEvtTreeWidgetItem;
+    delete m_pAlmEvtTreeWidgetItem;
+    delete m_pActEvtTreeWidgetItem;
+    delete m_pRemoteTripTreeWidgetItem;
+    delete m_pCheckEvtTreeWidgetItem;
+}
+
 void CResourceTreeWnd::CreateIEDTreeNodes()
 {
     m_pRootTreeWidgetItem  = new QTreeWidgetItem(this);
@@ -27,45 +57,105 @@ void CResourceTreeWnd::CreateIEDTreeNodes()
     SetNodeType(m_pRootTreeWidgetItem, Enum_Root_Node);
     this->addTopLevelItem(m_pRootTreeWidgetItem);
 
-    m_pAnaTreeWidgetItem  = new QTreeWidgetItem(m_pRootTreeWidgetItem);
-    m_pAnaTreeWidgetItem->setText( 0, "模拟量" );
-    SetNodeType(m_pAnaTreeWidgetItem, Enum_Ana_Node);
+    m_pInfoTreeWidgetItem  = new QTreeWidgetItem(m_pRootTreeWidgetItem);
+    m_pInfoTreeWidgetItem->setText( 0, "装置信息" );
+    SetNodeType(m_pInfoTreeWidgetItem, Enum_DeviceInfo_Node);
+
+    m_pParasTreeWidgetItem  = new QTreeWidgetItem(m_pRootTreeWidgetItem);
+    m_pParasTreeWidgetItem->setText( 0, "装置参数" );
+    SetNodeType(m_pParasTreeWidgetItem, Enum_DeviceParas_Node);
+
+    m_pSmpTreeWidgetItem  = new QTreeWidgetItem(m_pRootTreeWidgetItem);
+    m_pSmpTreeWidgetItem->setText( 0, "模拟量" );
+    SetNodeType(m_pSmpTreeWidgetItem, Enum_Ana_Node);
+
+    m_pAnaTreeWidgetItem  = new QTreeWidgetItem(m_pSmpTreeWidgetItem);
+    m_pAnaTreeWidgetItem->setText( 0, "AD模拟量" );
+    SetNodeType(m_pAnaTreeWidgetItem, Enum_Ana_AD_Node);
+
+    m_pDerivedTreeWidgetItem  = new QTreeWidgetItem(m_pSmpTreeWidgetItem);
+    m_pDerivedTreeWidgetItem->setText( 0, "衍生模拟量" );
+    SetNodeType(m_pDerivedTreeWidgetItem, Enum_Ana_Derived_Node);
+
+    m_pSvTreeWidgetItem  = new QTreeWidgetItem(m_pSmpTreeWidgetItem);
+    m_pSvTreeWidgetItem->setText( 0, "SV模拟量" );
+    SetNodeType(m_pSvTreeWidgetItem, Enum_Ana_SV_Node);
+
+    m_pGsAnaTreeWidgetItem  = new QTreeWidgetItem(m_pSmpTreeWidgetItem);
+    m_pGsAnaTreeWidgetItem->setText( 0, "GS模拟量" );
+    SetNodeType(m_pGsAnaTreeWidgetItem, Enum_Ana_GS_Node);
+
+    m_pOherAnaTreeWidgetItem  = new QTreeWidgetItem(m_pSmpTreeWidgetItem);
+    m_pOherAnaTreeWidgetItem->setText( 0, "其他模拟量" );
+    SetNodeType(m_pOherAnaTreeWidgetItem, Enum_Ana_Other_Node);
 
     m_pBITreeWidgetItem  = new QTreeWidgetItem(m_pRootTreeWidgetItem);
     m_pBITreeWidgetItem->setText( 0, "开关量" );
     SetNodeType(m_pBITreeWidgetItem, Enum_BI_Node);
 
-    m_pGSBITreeWidgetItem   = new QTreeWidgetItem(m_pRootTreeWidgetItem);
+    m_pGeneralBITreeWidgetItem   = new QTreeWidgetItem(m_pBITreeWidgetItem);
+    m_pGeneralBITreeWidgetItem->setText( 0, "硬开入" );
+    SetNodeType(m_pGeneralBITreeWidgetItem, Enum_BI_General_Node);
+
+    m_pSignalTreeWidgetItem   = new QTreeWidgetItem(m_pBITreeWidgetItem);
+    m_pSignalTreeWidgetItem->setText( 0, "软遥信" );
+    SetNodeType(m_pSignalTreeWidgetItem, Enum_BI_General_Node);
+
+    m_pGSBITreeWidgetItem   = new QTreeWidgetItem(m_pBITreeWidgetItem);
     m_pGSBITreeWidgetItem->setText( 0, "GS开入" );
     SetNodeType(m_pGSBITreeWidgetItem, Enum_GSBI_Node);
 
-    m_pGSBOTreeWidgetItem   = new QTreeWidgetItem(m_pRootTreeWidgetItem);
-    m_pGSBOTreeWidgetItem->setText( 0, "GS开出" );
-    SetNodeType(m_pGSBOTreeWidgetItem, Enum_GSBO_Node);
-
-    m_pTripMatrixTreeWidgetItem   = new QTreeWidgetItem(m_pRootTreeWidgetItem);
-    m_pTripMatrixTreeWidgetItem->setText( 0, "跳闸矩阵" );
-    SetNodeType(m_pTripMatrixTreeWidgetItem, Enum_TripMatrix_Node);
-
-    m_pSettingTreeWidgetItem   = new QTreeWidgetItem(m_pRootTreeWidgetItem);
-    m_pSettingTreeWidgetItem->setText( 0, "定值" );
-    SetNodeType(m_pSettingTreeWidgetItem, Enum_Setting_Node);
-
-    m_pSoftYBTreeWidgetItem   = new QTreeWidgetItem(m_pRootTreeWidgetItem);
+    m_pSoftYBTreeWidgetItem   = new QTreeWidgetItem(m_pBITreeWidgetItem);
     m_pSoftYBTreeWidgetItem->setText( 0, "软压板" );
     SetNodeType(m_pSoftYBTreeWidgetItem, Enum_SoftYB_Node);
 
-    m_pAlarmTreeWidgetItem   = new QTreeWidgetItem(m_pRootTreeWidgetItem);
-    m_pAlarmTreeWidgetItem->setText( 0, "告警记录" );
-    SetNodeType(m_pAlarmTreeWidgetItem, Enum_Alarm_Node);
+    m_pBOTreeWidgetItem   = new QTreeWidgetItem(m_pRootTreeWidgetItem);
+    m_pBOTreeWidgetItem->setText( 0, "开出" );
+    SetNodeType(m_pBOTreeWidgetItem, Enum_BO_Node);
 
-    m_pActTreeWidgetItem   = new QTreeWidgetItem(m_pRootTreeWidgetItem);
-    m_pActTreeWidgetItem->setText( 0, "动作记录" );
-    SetNodeType(m_pActTreeWidgetItem, Enum_Act_Node);
+    m_pGSBOTreeWidgetItem   = new QTreeWidgetItem(m_pBOTreeWidgetItem);
+    m_pGSBOTreeWidgetItem->setText( 0, "GS开出" );
+    SetNodeType(m_pGSBOTreeWidgetItem, Enum_GSBO_Node);
 
-//    m_pRemoteTreeWidgetItem   = new QTreeWidgetItem(m_pRootTreeWidgetItem);
-//    m_pRemoteTreeWidgetItem->setText( 0, "远方跳闸" );
-//    SetNodeType(m_pRemoteTreeWidgetItem, Enum_Remote_Node);
+    m_pTripMatrixTreeWidgetItem   = new QTreeWidgetItem(m_pBOTreeWidgetItem);
+    m_pTripMatrixTreeWidgetItem->setText( 0, "跳闸矩阵" );
+    SetNodeType(m_pTripMatrixTreeWidgetItem, Enum_TripMatrix_Node);
+
+    m_pYKTreeWidgetItem   = new QTreeWidgetItem(m_pBOTreeWidgetItem);
+    m_pYKTreeWidgetItem->setText( 0, "遥控" );
+    SetNodeType(m_pYKTreeWidgetItem, Enum_BO_YK_Node);
+
+    m_pSetTreeWidgetItem   = new QTreeWidgetItem(m_pRootTreeWidgetItem);
+    m_pSetTreeWidgetItem->setText( 0, "定值" );
+    SetNodeType(m_pSetTreeWidgetItem, Enum_Setting_Node);
+
+    m_pSPSetTreeWidgetItem   = new QTreeWidgetItem(m_pSetTreeWidgetItem);
+    m_pSPSetTreeWidgetItem->setText( 0, "不分组定值" );
+    SetNodeType(m_pSPSetTreeWidgetItem, Enum_SPSet_Node);
+
+    m_pSGSetTreeWidgetItem   = new QTreeWidgetItem(m_pSetTreeWidgetItem);
+    m_pSGSetTreeWidgetItem->setText( 0, "分组定值" );
+    SetNodeType(m_pSGSetTreeWidgetItem, Enum_SGSet_Node);
+
+    m_pEvtTreeWidgetItem   = new QTreeWidgetItem(m_pRootTreeWidgetItem);
+    m_pEvtTreeWidgetItem->setText( 0, "记录" );
+    SetNodeType(m_pEvtTreeWidgetItem, Enum_Evt_Node);
+
+    m_pAlmEvtTreeWidgetItem   = new QTreeWidgetItem(m_pEvtTreeWidgetItem);
+    m_pAlmEvtTreeWidgetItem->setText( 0, "告警记录" );
+    SetNodeType(m_pAlmEvtTreeWidgetItem, Enum_Alarm_Node);
+
+    m_pActEvtTreeWidgetItem   = new QTreeWidgetItem(m_pEvtTreeWidgetItem);
+    m_pActEvtTreeWidgetItem->setText( 0, "动作记录" );
+    SetNodeType(m_pActEvtTreeWidgetItem, Enum_Act_Node);
+
+    m_pRemoteTripTreeWidgetItem   = new QTreeWidgetItem(m_pEvtTreeWidgetItem);
+    m_pRemoteTripTreeWidgetItem->setText( 0, "远方跳闸" );
+    SetNodeType(m_pRemoteTripTreeWidgetItem, Enum_Remote_Node);
+
+    m_pCheckEvtTreeWidgetItem   = new QTreeWidgetItem(m_pEvtTreeWidgetItem);
+    m_pCheckEvtTreeWidgetItem->setText( 0, "自检记录" );
+    SetNodeType(m_pCheckEvtTreeWidgetItem, Enum_Check_Node);
 
     expandItem(m_pRootTreeWidgetItem);
 }
@@ -99,44 +189,12 @@ void CResourceTreeWnd::ShowCurrentNodeInfoWnd()
     QTreeWidgetItem* pCurrentItem = currentItem();
     Enum_Node_Type nNodeType = GetNodeType(pCurrentItem);
 
-    if (nNodeType == Enum_Root_Node)
-    {
-        CCfgMainWnd::Instance()->ShowRootInfo();
-        return;
-    }
-
     switch(nNodeType)
     {
     case Enum_Ana_Node:
         CCfgMainWnd::Instance()->ShowAnaInfo();
         break;
-    case Enum_BI_Node:
-        CCfgMainWnd::Instance()->ShowBIInfo();
-        break;
-    case Enum_GSBI_Node:
-        CCfgMainWnd::Instance()->ShowGSBIInfo();
-        break;
-    case Enum_GSBO_Node:
-        CCfgMainWnd::Instance()->ShowGSBOInfo();
-        break;
-    case Enum_TripMatrix_Node:
-        CCfgMainWnd::Instance()->ShowTripMatrixInfo();
-        break;
-    case Enum_Setting_Node:
-        CCfgMainWnd::Instance()->ShowSettingInfo();
-        break;
-    case Enum_SoftYB_Node:
-        CCfgMainWnd::Instance()->ShowSoftYBInfo();
-        break;
-    case Enum_Alarm_Node:
-        CCfgMainWnd::Instance()->ShowAlarmInfo();
-        break;
-    case Enum_Act_Node:
-        CCfgMainWnd::Instance()->ShowActInfo();
-        break;
-    case Enum_Remote_Node:
-        CCfgMainWnd::Instance()->ShowRemoteInfo();
-        break;
+        //TODO
     default:
         CCfgMainWnd::Instance()->ShowDefaultInfo();
         break;
--- App/CResourceTreeWnd.h
+++ App/CResourceTreeWnd.h
@@ -17,28 +17,57 @@ public:
                                                     //	    |
         Enum_DeviceInfo_Node,						//		|-装置信息
                                                     //  	|
+        Enum_DeviceParas_Node,						//		|-装置参数
+                                                    //  	|
         Enum_Ana_Node,								//		|-模拟量
                                                     //		|
+        Enum_Ana_AD_Node,                           //      |           |-AD模拟量
+                                                    //      |           |
+        Enum_Ana_Derived_Node,                      //      |           |-衍生模拟量
+                                                    //      |           |
+        Enum_Ana_SV_Node,                           //      |           |-SV模拟量
+                                                    //      |           |
+        Enum_Ana_GS_Node,                           //      |           |-GS模拟量
+                                                    //      |           |
+        Enum_Ana_Other_Node,                        //      |           |-其他模拟量
+                                                    //      |           |
         Enum_BI_Node,                               //		|-开关量
                                                     //		|
-        Enum_GSBI_Node,                     		//		|-GS开入
+        Enum_BI_General_Node,                       //      |           |-硬开入
+                                                    //      |           |
+        Enum_BI_Signal_Node,                        //      |           |-软遥信
+                                                    //      |           |
+        Enum_GSBI_Node,                     		//		|           |-GS开入
+                                                    //		|           |
+        Enum_SoftYB_Node,                			//		|           |-软压板
+                                                    //		|           |
+        Enum_BO_Node,								//		|-开出
                                                     //		|
-        Enum_GSBO_Node,								//		|-GS开出
-                                                    //		|
-        Enum_TripMatrix_Node,						//		|-跳闸矩阵
-                                                    //		|
+        Enum_GSBO_Node,								//		|           |-GS开出
+                                                    //		|           |
+        Enum_TripMatrix_Node,						//		|           |-跳闸矩阵
+                                                    //		|           |
+        Enum_BO_YK_Node,                            //		|           |-遥控
+                                                    //		|           |
         Enum_Setting_Node,                      	//		|-定值
                                                     //		|
-        Enum_SoftYB_Node,                			//		|-告警
+        Enum_SPSet_Node,                            //		|           |-不分组定值
+                                                    //		|           |
+        Enum_SGSet_Node,                            //		|           |-分组定值
+                                                    //		|           |
+        Enum_Evt_Node,                              //		|-告警
                                                     //		|
-        Enum_Alarm_Node,        					//		|-告警
-                                                    //		|
-        Enum_Act_Node,      						//		|-动作
-                                                    //		|
-        Enum_Remote_Node,							//		|-远方跳闸
+        Enum_Alarm_Node,        					//		|           |-告警
+                                                    //		|           |
+        Enum_Act_Node,      						//		|           |-动作
+                                                    //		|           |
+        Enum_Remote_Node,							//		|           |-远方跳闸
+                                                    //		|           |
+        Enum_Check_Node,							//		|           |-自检
     };
 
     CResourceTreeWnd(QWidget *parent);
+    ~CResourceTreeWnd();
 
     void CreateIEDTreeNodes();
     void ClearTreeNode();
@@ -52,17 +81,31 @@ private:
     CDataBase * m_pXerceXML;
 
     QTreeWidgetItem * m_pRootTreeWidgetItem;
+    QTreeWidgetItem * m_pInfoTreeWidgetItem;
+    QTreeWidgetItem * m_pParasTreeWidgetItem;
+    QTreeWidgetItem * m_pSmpTreeWidgetItem;
     QTreeWidgetItem * m_pAnaTreeWidgetItem;
+    QTreeWidgetItem * m_pDerivedTreeWidgetItem;
+    QTreeWidgetItem * m_pSvTreeWidgetItem;
+    QTreeWidgetItem * m_pGsAnaTreeWidgetItem;
+    QTreeWidgetItem * m_pOherAnaTreeWidgetItem;
     QTreeWidgetItem * m_pBITreeWidgetItem;
+    QTreeWidgetItem * m_pGeneralBITreeWidgetItem;
+    QTreeWidgetItem * m_pSignalTreeWidgetItem;
     QTreeWidgetItem * m_pGSBITreeWidgetItem;
+    QTreeWidgetItem * m_pSoftYBTreeWidgetItem;
+    QTreeWidgetItem * m_pBOTreeWidgetItem;
     QTreeWidgetItem * m_pGSBOTreeWidgetItem;
     QTreeWidgetItem * m_pTripMatrixTreeWidgetItem;
-    QTreeWidgetItem * m_pSettingTreeWidgetItem;
-    QTreeWidgetItem * m_pSoftYBTreeWidgetItem;
-    QTreeWidgetItem * m_pAlarmTreeWidgetItem;
-    QTreeWidgetItem * m_pActTreeWidgetItem;
-    QTreeWidgetItem * m_pRemoteTreeWidgetItem;
-
+    QTreeWidgetItem * m_pYKTreeWidgetItem;
+    QTreeWidgetItem * m_pSetTreeWidgetItem;
+    QTreeWidgetItem * m_pSPSetTreeWidgetItem;
+    QTreeWidgetItem * m_pSGSetTreeWidgetItem;
+    QTreeWidgetItem * m_pEvtTreeWidgetItem;
+    QTreeWidgetItem * m_pAlmEvtTreeWidgetItem;
+    QTreeWidgetItem * m_pActEvtTreeWidgetItem;
+    QTreeWidgetItem * m_pRemoteTripTreeWidgetItem;
+    QTreeWidgetItem * m_pCheckEvtTreeWidgetItem;
 private slots:
     void currentItemChangedSlot ( QTreeWidgetItem * current, QTreeWidgetItem * previous );
 
